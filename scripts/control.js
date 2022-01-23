0c4ca8e03b02a9b62ed187847b0cac1f4f4e39ef
khushijain3103 <khushijain312003@gmail.com>
2022-01-24 00:41:24 +0530

    added some more features

--- scripts/control.js
+++ scripts/control.js
@@ -5,8 +5,28 @@ function eventbinder(){
     compute.addEventListener('click',calculate);
 };
 
+function validate(){
+    if(!allowances.basicSalary || allowances.basicSalary<0)
+    {
+        var fieldset = document.querySelector('#fieldset');
+        fieldset.innerHTML=" ";
+        var span = document.createElement('span');
+        span.innerText = "PLEASE ENTER YOUR SALARY PROPERLY!";
+        fieldset.appendChild(span);
+        return true;
+    
+
+    }
+    return false;
+    
+}
+
 function calculate(){
     allowances.basicSalary = parseInt(document.getElementById('BS').value);
+    if(validate()===true)
+    {
+        return;
+    }
     // console.log(allowances.basicSalary);
     // dry princple which means do not repeat yourself is violated here so instead we shoule do line no 20th code
     // document.getElementById("hra").innerText = allowances.hra();
@@ -26,26 +46,19 @@ function calculate(){
     // }
 
     var fieldset = document.querySelector('#fieldset');
-    
+    fieldset.innerHTML=" ";
     for(let key in allowances)
     {
-        // console.log(allowances[key]);
         
         if(typeof(allowances[key])==='function')
         {
-            console.log(allowances[key]);
             var para = document.createElement('p');
 
             para.innerHTML = `${(allowances[key].name).toUpperCase()} is &#8377 ${allowances[key]()}`;
             fieldset.appendChild(para);
         }
-        else{
-            console.log("no");
-        }
         
     }
-
-
 }
 
 
